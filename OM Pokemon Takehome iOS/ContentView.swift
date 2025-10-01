3d61055e7dc71fbaa81ae9102fd7b9ba4eb15b0a
Kevin Lagat <klagat@oldmutual.com>
2025-10-01 18:44:17 +0300

    Initial Commit

---
+++ OM Pokemon Takehome iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OM Pokemon Takehome iOS
+//
+//  Created by Kevin Lagat Home PC on 01/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OM Pokemon Takehome iOS/OM_Pokemon_Takehome_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OM_Pokemon_Takehome_iOSApp.swift
+//  OM Pokemon Takehome iOS
+//
+//  Created by Kevin Lagat Home PC on 01/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct OM_Pokemon_Takehome_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
